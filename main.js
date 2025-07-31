211c3f2b58c28800bf21d8278d5d17c3ccbc1977
hosein mohammadi <hosein011dev@gmail.com>
2025-08-01 01:48:14 +0330

    debug cvv js

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 const cardNumberInp = document.querySelector(".card-number > input")
+const cardShow = document.querySelectorAll(".card-box >div:nth-of-type(2) > input")
 const cardHolder = document.querySelector(".card-holder > input")
+const holder = document.querySelector("#holder")
 const month = document.querySelector("#month")
 const years = document.querySelector("#years")
 const cvv = document.querySelector("#cvv")
-const submitBtn = document.querySelector("form > input[type='submit']")
-const cardShow = document.querySelectorAll(".card-box >div:nth-of-type(2) > input")
-const holder = document.querySelector("#holder")
-const cardBox = document.querySelector(".card-box")
-const cardBack = document.querySelector("#cardBoxBack")
 const cvvshow = document.querySelector("#cvvShow")
-let cNum = []
+// const cardBox = document.querySelector(".card-box")
+// const cardBack = document.querySelector("#cardBoxBack")
+// const submitBtn = document.querySelector("form > input[type='submit']")
+
+let cNum = null
 cardNumberInp.addEventListener("input", () => {
     let cNum = cardNumberInp.value.trim().slice(0, 16);
-    let cNum2 = cardNumberInp.value.slice(4, 8)
-    let cNum3 = cardNumberInp.value.slice(8, 12)
-    let cNum4 = cardNumberInp.value.slice(12, 16)
-    console.log(cNum);
-
-    console.log(cNum.length);
+    let cNum2 = cardNumberInp.value.trim().slice(4, 8)
+    let cNum3 = cardNumberInp.value.trim().slice(8, 12)
+    let cNum4 = cardNumberInp.value.trim().slice(12, 16)
 
     if (cNum.length <= 4) {
         cardShow[0].value = cNum
@@ -39,23 +37,38 @@ cardHolder.addEventListener("input", () => {
     holder.textContent = cardHolder.value
 })
 
-
+    
 month.addEventListener('change', (e) => {
     document.querySelector("#monthShow").textContent = e.target.value
 })
 years.addEventListener('change', (e) => {
-    let val = e.target.value;
-    document.querySelector("#yearsShow").textContent = val;
+    document.querySelector("#yearsShow").textContent = e.target.value;
 });
-const cardContainer = document.querySelector(".card-container");
 
-cvv.addEventListener("focus", () => {
-    cardContainer.classList.toggle("flipped");
+let isFlipped = false;
+const cardContainer = document.querySelector(".card-container");
+cvv.addEventListener("click", () => {
+    if(!isFlipped){
+        cardContainer.classList.toggle("flipped");
+        isFlipped = true
+    }
 });
 
 cvv.addEventListener("input", (e) => {
     let x = cvv.value.slice(0, 4)
-    cvvshow.innerHTML = x
+    cvvshow.textContent = x
 
 })
+
+
+const otherInp = document.querySelectorAll('.otherInp')
+
+otherInp.forEach(input => {
+    input.addEventListener('click',()=>{
+        if(isFlipped){
+        cardContainer.classList.toggle("flipped");
+            isFlipped = false
+        }
+    })
+});
 
